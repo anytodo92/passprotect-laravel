6745f168a5630e12aeb3de9fe5912a7c17ec000b
anytodo92 <anything.todo292@gmail.com>
2023-11-24 03:15:20 +0100

    added forgot password, chagne password features

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,8 @@ use App\Heplers\SecurePassword;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -13,7 +15,7 @@ class AuthController extends Controller
      * Handle an authentication attempt.
      */
     public function login(Request $request): JsonResponse {
-        if ($request->user('sanctum')) {
+        if (Auth::guard('sanctum')->user()) {
             return response()->json([
                 'message' => 'Already authenticated'
             ], 201);
@@ -49,7 +51,7 @@ class AuthController extends Controller
     }
 
     public function register(Request $request): JsonResponse {
-        if ($request->user('sanctum')) {
+        if (Auth::guard('sanctum')->user()) {
             return response()->json([
                 'message' => 'Already authenticated'
             ], 201);
@@ -114,13 +116,76 @@ class AuthController extends Controller
         ]);
     }
 
-    public function resetPassword(Request $request): JsonResponse {
+    public function resetPassword(Request $request, string $token): JsonResponse {
+        if (Auth::guard('sanctum')->user()) {
+            return response()->json([
+                'message' => 'Already authenticated',
+                'token' => $token
+            ], 201);
+        }
         return response()->json([]);
     }
 
     public function forgotPassword(Request $request): JsonResponse {
+        if (Auth::guard('sanctum')->user()) {
+            return response()->json([
+                'message' => 'Already authenticated'
+            ], 201);
+        }
+
+        $request->validate([
+            'email' => ['required', 'email']
+        ]);
+
+        $email = $request->string('email');
+        $user = User::where('user_email', '=', $email)->get();
+        if (!$user) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, no account with that email exists. Please try again...'
+            ]);
+        }
+
+        $url = env('FRONTEND_SITE_URL').'/reset-password/'.Hash::make();
+        //Todo: Sending email
+
         return response()->json([]);
     }
 
+    public function changePassword(Request $request): JsonResponse {
+        $request->validate([
+            'old_password' => ['required'],
+            'new_password' => ['required', 'min:8']
+        ]);
 
+        $user = Auth::guard('sanctum')->user();
+        $oldPassword = $request->string('old_password')->trim()->value();
+        $newPassword = $request->string('new_password')->trim()->value();
+
+        if (!SecurePassword::check($oldPassword, $user->user_password_hash)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Please enter correct old password'
+            ]);
+        }
+
+        $user->user_password_hash = SecurePassword::make($newPassword, PASSWORD_BCRYPT_C);
+        if (!$user->save()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Operation is failed'
+            ]);
+        }
+
+        return response()->json([
+            'success' => true
+        ]);
+    }
+
+    public function logout() {
+        auth()->user()->tokens()->delete();
+        return resonse()->json([
+            'success' => true
+        ]);
+    }
 }
