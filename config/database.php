d587a74da41cbe0d113cd6ccb5352e12009321f5
anytodo92 <anything.todo292@gmail.com>
2023-12-19 15:19:53 +0100

    made some changes

--- config/database.php
+++ config/database.php
@@ -49,7 +49,7 @@ return [
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_PASSDROPIT_DATABASE', 'passdropit'),
-            'username' => env('DB_PASSDROPTI_USERNAME', 'root'),
+            'username' => env('DB_PASSDROPIT_USERNAME', 'root'),
             'password' => env('DB_PASSDROPIT_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
